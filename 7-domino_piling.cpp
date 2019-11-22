e577985240d24fd95b234ab36c627be39602e45c
fedas duger <harikrishmenon@gmail.com>
2019-11-22 23:21:11 +0530

    completed 7

---
+++ 7-domino_piling.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef int64_t i64;
+
+int main(void)
+{
+    i64 N, M;
+    cin >> N >> M;
+    i64 ans = N * M / 2;
+    cout << ans;
+    return 0;
+}
